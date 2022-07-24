f1d33761f291ae2344a5198301e0ebc595ab5034
muhab404 <83089239+muhab404@users.noreply.github.com>
2022-07-24 07:10:57 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3),
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
